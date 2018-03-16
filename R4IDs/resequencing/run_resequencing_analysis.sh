019c4dccaa623c2cc5c397652280d94d863c99f4
lonelyjoeparker <joe@lonelyjoeparker.com>
2018-03-16 00:59:30 +0000

    debug containers for R4IDs deploy

--- R4IDs/resequencing/run_resequencing_analysis.sh
+++ R4IDs/resequencing/run_resequencing_analysis.sh
@@ -1,8 +1,23 @@
 pwd
-ls
-echo 'try find'
-find . -name sh
-#java -version  
-#/bin/java -version  
-#/usr/bin/java -version
-blastn -h
+echo 'trigger resequencing run'
+echo 'list input dir'
+ls -l /input_resequencing
+echo 'list input arg probably'
+ls -l /input_resequencing/$1
+echo 'list db'
+ls -l /blast_db
+echo 'list output arg'
+ls -l $2
+echo 'list output dir'
+ls -l /output_web
+echo 'whats in the conf file?'
+for db in $(cat /blast_db/blast_db.conf)
+do
+db_name_only=$(basename $db)
+echo 'blast conf line DB ' $db
+echo "blast command::: blastn -db $db -query /input_resequencing/$1 -num_alignments 1 -outfmt '6 sacc qacc length pidents evalue' >/output_web/results.blast.$i.out"
+blastn -db $db -query /input_resequencing/$1 -num_alignments 1 -outfmt "6 qacc length pident evalue" | /app/sum_blast.pl - > /output_web/agg.blasts.$db_name_only
+done
+# cat the outputs to web output
+cat /output_web/agg.blasts* >> /output_web/data.tdf
+
--- R4IDs/training/setup_training_data.sh
+++ R4IDs/training/setup_training_data.sh
@@ -7,21 +7,22 @@ blastn -version
 # work with volumes - extra stuff from john/james
 # https://github.com/RBGKew/demo-docker-blast/blob/master/blast.sh
 
-if [ -f /input ]
+if [ -f /input_training ]
 then
   #blastn -query /input -db /db/fish.fa -task blastn -out /output/output
   head /input
-elif [ -d /input ]
+elif [ -d /input_training ]
 then
-  for file in /input/*;
+  for file in /input_training/*;
   do
-    filename=$(basename $file)
+    #filename=$(basename $file)
+    filename=$(basename "${file%.*}")
     #blastn -query $file -db /db/fish.fa -task blastn -out /output/${filename}.out
-    head -n 2 $file > /output/${filename}.out
+    head -n 2 $file > /blast_db/${filename}.out
     # do the actual work
     # attempt to create a blast DB from the input
     # if exit 0, add DB path/name to the databases list
-    makeblastdb -dbtype nucl -in $file -out /output/DB.${filename}
-    echo /output/DB.${filename} >> /output/blast_db.conf
+    makeblastdb -dbtype nucl -in $file -out /blast_db/DB.${filename}.DB
+    echo /blast_db/DB.${filename}.DB >> /blast_db/blast_db.conf
   done
 fi
