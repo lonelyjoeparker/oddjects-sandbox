4b78fc66d3966352d60f32a92bfd5cac63e4267c
lonelyjoeparker <joe@lonelyjoeparker.com>
2024-02-26 22:22:29 +0000

    implemented proper read coverage depth plotting by argument

--- utils/genome_annotation_tests/genome_depth_plot_utils.py
+++ utils/genome_annotation_tests/genome_depth_plot_utils.py
@@ -5,6 +5,22 @@ BASED ON
 example in https://github.com/Edinburgh-Genome-Foundry/DnaFeaturesViewer/blob/master/examples/with_gc_plot.py
 @Author @lonelyjoeparker
 @Date 2024-02-07
+
+USAGE:
+    record = SeqIO.read("example_sequence.gb", "genbank")
+    translator = BiopythonTranslator()
+    graphic_record = translator.translate_record(record)
+
+    fig, (ax1, ax2, ax3) = plt.subplots(
+        3, 1, figsize=(10, 3), sharex=True, gridspec_kw={"height_ratios": [4, 1, 1]}
+    )
+    graphic_record.plot(ax=ax1, with_ruler=False, strand_in_label_threshold=4)
+    plot_local_gc_content(record, window_size=50, ax=ax2)
+    plot_depth_content(record, window_size=50, ax=ax3, depthfile='example_coverage_data.tdf')
+
+    fig.tight_layout()  # Resize the figure to the right height
+    fig.savefig("with_gc_plot2.png")
+
 """
 import matplotlib.pyplot as plt
 from dna_features_viewer import BiopythonTranslator
@@ -42,17 +58,3 @@ def plot_depth_content(record, window_size, ax, depthfile):
     ax.fill_between(xx, yy, alpha=0.3)
     ax.set_ylim(bottom=0)
     ax.set_ylabel("DEPTH")
-
-record = SeqIO.read("example_sequence.gb", "genbank")
-translator = BiopythonTranslator()
-graphic_record = translator.translate_record(record)
-
-fig, (ax1, ax2, ax3) = plt.subplots(
-    3, 1, figsize=(10, 3), sharex=True, gridspec_kw={"height_ratios": [4, 1, 1]}
-)
-graphic_record.plot(ax=ax1, with_ruler=False, strand_in_label_threshold=4)
-plot_local_gc_content(record, window_size=50, ax=ax2)
-plot_depth_content(record, window_size=50, ax=ax3, depthfile='example_coverage_data.tdf')
-
-fig.tight_layout()  # Resize the figure to the right height
-fig.savefig("with_gc_plot2.png")
